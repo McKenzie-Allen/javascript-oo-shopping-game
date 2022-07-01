5ddd7d4715ced6770a8fd32a123452643cc7bc8f
McKenzie Allen <mckenzie.allen144@gmail.com>
2022-06-30 19:54:16 -0600

    method now added

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -7,7 +7,17 @@ const items = 0
 let player = {
     name,
     score,
-    items
+    items,
+    getCurrentScore(){
+        return this.score
+    },
+    addPoints(points){
+       this.score = this.score + points 
+    },
+    deductPoints(points){
+        this.score = this.score - points
+    },
+
 }
 // Define the Product class - write the Constructor function for Product class here
 
