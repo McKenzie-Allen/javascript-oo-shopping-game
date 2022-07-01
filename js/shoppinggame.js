0b16ae1e9c1dfefb95e53d690003ad6471ea570a
McKenzie Allen <mckenzie.allen144@gmail.com>
2022-06-30 20:37:27 -0600

    added magic product

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -42,9 +42,15 @@ Object.defineProperty(Product.prototype, "daysToExpire", {
     }
 })
 // Add method getDetails to Product here
-
+Product.prototype.getDetails = function () {
+   return `Product Name: ${this.name} , Product Price: ${this.price}`
+}
 // Define the MagicProduct class here
-
+function MagicProduct(id, name, price, expiryDate, points, isBonus) {
+    Product.call(this, id, name, price, expiryDate)
+        this.points = points;
+        this.isBonus = isBonus
+}
 // Establish inheritance between Product() & MagicProduct() here
 
 // Define Rating class here
